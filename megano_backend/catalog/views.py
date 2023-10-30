5997e89c6ac7cc89437aac21ecf84112b1c6d2bf
Roman <31464179+grand-roman@users.noreply.github.com>
2023-10-30 21:44:52 +0700

    fix

--- megano_backend/catalog/views.py
+++ megano_backend/catalog/views.py
@@ -1,6 +1,5 @@
 from django.db.models import Count, F
 from django.http import JsonResponse
-from django.http import JsonResponse
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,7 +7,7 @@ from rest_framework.request import Request
 from .models import Category, Tag, Product, Sale
 from .pagination import CatalogPagination
 from .serializers import ProductSerializer, TagSerializer, CategorySerializer, ReviewSerializer, SaleSerializer
-
+import json
 
 # class CategoryListView(APIView):
 #     """Класс для отображения списка категорий и их подкатегорий"""
@@ -17,6 +16,211 @@ from .serializers import ProductSerializer, TagSerializer, CategorySerializer, R
 #         categories = Category.objects.filter(parent=None).prefetch_related("subcategories")
 #         serialized = CategorySerializer(categories, many=True)
 #         return JsonResponse(serialized.data)
+def banners(request):
+	data = [
+		{
+			"id": "123",
+			"category": 55,
+			"price": 500.67,
+			"count": 12,
+			"date": "Thu Feb 09 2023 21:39:52 GMT+0100 (Central European Standard Time)",
+			"title": "video card",
+			"description": "description of the product",
+			"freeDelivery": True,
+			"images": [
+				{
+					"src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
+					"alt": "any alt text",
+				}
+			],
+			"tags": [
+				"string"
+			],
+			"reviews": 5,
+			"rating": 4.6
+		},
+	]
+	return JsonResponse(data, safe=False)
+
+
+def basket(request):
+	if(request.method == "GET"):
+		print('[GET] /api/basket/')
+		data = [
+			{
+				"id": 123,
+				"category": 55,
+				"price": 500.67,
+				"count": 12,
+				"date": "Thu Feb 09 2023 21:39:52 GMT+0100 (Central European Standard Time)",
+				"title": "video card",
+				"description": "description of the product",
+				"freeDelivery": True,
+				"images": [
+						{
+							"src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
+							"alt": "hello alt",
+						}
+				 ],
+				 "tags": [
+						{
+							"id": 0,
+							"name": "Hello world"
+						}
+				 ],
+				"reviews": 5,
+				"rating": 4.6
+			},
+			{
+				"id": 124,
+				"category": 55,
+				"price": 201.675,
+				"count": 5,
+				"date": "Thu Feb 09 2023 21:39:52 GMT+0100 (Central European Standard Time)",
+				"title": "video card",
+				"description": "description of the product",
+				"freeDelivery": True,
+				"images": [
+						{
+							"src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
+							"alt": "hello alt",
+						}
+				 ],
+				 "tags": [
+						{
+							"id": 0,
+							"name": "Hello world"
+						}
+				 ],
+				"reviews": 5,
+				"rating": 4.6
+			}
+		]
+		return JsonResponse(data, safe=False)
+
+	elif (request.method == "POST"):
+		body = json.loads(request.body)
+		id = body['id']
+		count = body['count']
+		print('[POST] /api/basket/   |   id: {id}, count: {count}'.format(id=id, count=count))
+		data = [
+			{
+				"id": id,
+				"category": 55,
+				"price": 500.67,
+				"count": 13,
+				"date": "Thu Feb 09 2023 21:39:52 GMT+0100 (Central European Standard Time)",
+				"title": "video card",
+				"description": "description of the product",
+				"freeDelivery": True,
+				"images": [
+						{
+							"src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
+							"alt": "hello alt",
+						}
+				 ],
+				 "tags": [
+						{
+							"id": 0,
+							"name": "Hello world"
+						}
+				 ],
+				"reviews": 5,
+				"rating": 4.6
+			}
+		]
+		return JsonResponse(data, safe=False)
+
+	elif (request.method == "DELETE"):
+		body = json.loads(request.body)
+		id = body['id']
+		print('[DELETE] /api/basket/')
+		data = [
+			{
+			"id": id,
+			"category": 55,
+			"price": 500.67,
+			"count": 11,
+			"date": "Thu Feb 09 2023 21:39:52 GMT+0100 (Central European Standard Time)",
+			"title": "video card",
+			"description": "description of the product",
+			"freeDelivery": True,
+			"images": [
+					{
+						"src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
+						"alt": "hello alt",
+					}
+			 ],
+			 "tags": [
+					{
+						"id": 0,
+						"name": "Hello world"
+					}
+			 ],
+			"reviews": 5,
+			"rating": 4.6
+			}
+		]
+		return JsonResponse(data, safe=False)
+
+
+def productsPopular(request):
+	data = [
+		{
+			"id": "123",
+			"category": 55,
+			"price": 500.67,
+			"count": 12,
+			"date": "Thu Feb 09 2023 21:39:52 GMT+0100 (Central European Standard Time)",
+			"title": "video card",
+			"description": "description of the product",
+			"freeDelivery": True,
+			"images": [
+					{
+						"src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
+						"alt": "hello alt",
+					}
+			 ],
+			 "tags": [
+					{
+						"id": 0,
+						"name": "Hello world"
+					}
+			 ],
+			"reviews": 5,
+			"rating": 4.6
+		}
+	]
+	return JsonResponse(data, safe=False)
+
+def productsLimited(request):
+	data = [
+		{
+			"id": "123",
+			"category": 55,
+			"price": 500.67,
+			"count": 12,
+			"date": "Thu Feb 09 2023 21:39:52 GMT+0100 (Central European Standard Time)",
+			"title": "video card",
+			"description": "description of the product",
+			"freeDelivery": True,
+			"images": [
+					{
+						"src": "https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-14.jpg",
+						"alt": "hello alt",
+					}
+			 ],
+			 "tags": [
+					{
+						"id": 0,
+						"name": "Hello world"
+					}
+			 ],
+			"reviews": 5,
+			"rating": 4.6
+		}
+	]
+	return JsonResponse(data, safe=False)
 
 
 class CategoryListView(ListAPIView):
