e80b3c8bd266b319fa566cd547293c66d894508a
Евгений Соловей <e.v.solovey@inbox.ru>
2023-11-14 01:38:15 +0300

    cart app

--- megano_backend/basket/urls.py
+++ megano_backend/basket/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from basket.views import CartView
 
 urlpatterns = [
-    path("basket", CartView.as_view(), name="basket"),
+    path("basket/", CartView.as_view(), name="basket"),
 ]
