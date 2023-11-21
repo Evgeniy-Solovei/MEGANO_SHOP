cd4f0809fed5da98e06494e47abf3f288ee3d51d
Евгений Соловей <e.v.solovey@inbox.ru>
2023-11-21 14:45:12 +0300

    Swager functionality implemented

--- megano_backend/order/urls.py
+++ megano_backend/order/urls.py
@@ -4,5 +4,5 @@ from order.views import OrdersView, OrderDetailsView, PaymentOrderView
 urlpatterns = [
     path("orders/", OrdersView.as_view(), name="orders"),
     path("orders/<int:pk>/", OrderDetailsView.as_view(), name="order"),
-    path("payment/", PaymentOrderView.as_view(), name="order"),
+    path("payment/<int:pk>/", PaymentOrderView.as_view(), name="order"),
 ]
